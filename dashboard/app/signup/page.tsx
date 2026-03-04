7344d3675083a3a93754000c5bcbe91a3796ce3b
Sargam Poudel <sargampoudel100@gmail.com>
2026-03-04 16:58:23 +0530

    fix: redirects

--- dashboard/app/signup/page.tsx
+++ dashboard/app/signup/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
-import { SignupForm } from "@/components/signup-form"
+import { SignupForm } from "@/components/signup-form";
 import { getServerSession } from "@/lib/server-auth";
+import Link from "next/link";
 
 export default async function SignupPage() {
   const session = await getServerSession();
@@ -13,10 +14,15 @@ export default async function SignupPage() {
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex items-center justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <img src="/logo.jpg" alt="" aria-hidden="true" className="size-6 rounded-md object-cover" />
+          <Link href="/" className="flex items-center gap-2 font-medium">
+            <img
+              src="/logo.jpg"
+              alt=""
+              aria-hidden="true"
+              className="size-6 rounded-md object-cover"
+            />
             Actumx
-          </a>
+          </Link>
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
@@ -32,5 +38,5 @@ export default async function SignupPage() {
         />
       </div>
     </div>
-  )
+  );
 }
